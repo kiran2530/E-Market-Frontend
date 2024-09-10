ad410d1136a99e76b994c4baa27330b3fb6ecd26
kirannikam <nikamsarkar464@gmail.com>
2024-09-10 13:51:07 +0530

    created landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
-import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/home/Home'
+import Navbar from './components/navbar/Navbar'
+import './App.css'
+import React from 'react'
 
 function App () {
   return (
     <Router>
-      <div className='body-container'>
+      <div className='app-container'>
+        <Navbar />
         <Home />
       </div>
     </Router>
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,41 +1,87 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Home.module.css' // Importing the CSS module
+import homeHeader from '../../assets/home-header.png'
 
 const Home = () => {
   return (
     <div className={styles.homeContainer}>
       <header className={styles.homeHeader}>
-        <h1 className={styles.homeTitle}>Welcome to Our E-Commerce Platform</h1>
-        <p className={styles.homeDescription}>
-          Find the best products from trusted vendors and enjoy a seamless
-          shopping experience.
-        </p>
-        <div className={styles.homeButtons}>
-          <Link to='/shop' className={`${styles.btn} ${styles.btnPrimary}`}>
-            Shop Now
-          </Link>
-          <Link to='/signup' className={`${styles.btn} ${styles.btnSecondary}`}>
-            Get Started
-          </Link>
+        <div className={styles.textContainer}>
+          <h1 className={styles.homeTitle}>
+            Welcome to Our <br />{' '}
+            <strong style={{ color: 'blue' }}>E-Market</strong> Platform
+          </h1>
+          <p className={styles.homeDescription}>
+            Find the best products from trusted vendors and enjoy a seamless
+            shopping experience.
+          </p>
+          <div className={styles.homeButtons}>
+            <Link to='/shop' className={`${styles.btn} ${styles.btnPrimary}`}>
+              Shop Now
+            </Link>
+            <Link
+              to='/signup'
+              className={`${styles.btn} ${styles.btnSecondary}`}
+            >
+              Get Started
+            </Link>
+          </div>
+        </div>
+
+        {/* New image */}
+        <div className={styles.imageContainer}>
+          <img
+            src={homeHeader}
+            alt='Farmers Market'
+            className={styles.homeImage}
+          />
         </div>
       </header>
-      <section className={styles.homeFeatures}>
-        <div className={styles.feature}>
-          <h2>For Buyers</h2>
-          <p>
-            Browse through a wide range of products, manage your cart, and check
-            out with ease.
-          </p>
-        </div>
-        <div className={styles.feature}>
-          <h2>For Vendors</h2>
-          <p>
-            Manage your products, track sales, and connect with potential buyers
-            effortlessly.
-          </p>
+
+      <section className={styles.featureContainer}>
+        <h1 className={styles.homeTitle}>
+          Our <strong style={{ color: 'blue' }}>Services</strong>
+        </h1>
+        <div className={styles.homeFeatures}>
+          <div className={styles.feature}>
+            <h2>For Buyers</h2>
+            <p>
+              Browse through a wide range of products, manage your cart, and
+              check out with ease.
+            </p>
+          </div>
+          <div className={styles.feature}>
+            <h2>For Vendors</h2>
+            <p>
+              Manage your products, track sales, and connect with potential
+              buyers effortlessly.
+            </p>
+          </div>
+          <div className={styles.feature}>
+            <h2>For Vendors</h2>
+            <p>
+              Manage your products, track sales, and connect with potential
+              buyers effortlessly.
+            </p>
+          </div>
+          <div className={styles.feature}>
+            <h2>For Vendors</h2>
+            <p>
+              Manage your products, track sales, and connect with potential
+              buyers effortlessly.
+            </p>
+          </div>
+          <div className={styles.feature}>
+            <h2>For Vendors</h2>
+            <p>
+              Manage your products, track sales, and connect with potential
+              buyers effortlessly.
+            </p>
+          </div>
         </div>
       </section>
+
       <footer className={styles.homeFooter}>
         <p>
           &copy; {new Date().getFullYear()} E-Commerce Platform. All rights
