5b2f88b3dfebedda40e3f1a28670b7d67386cbc2
kirannikam <nikamsarkar464@gmail.com>
2024-09-10 23:58:41 +0530

    update home

--- src/components/common/home/Home.jsx
+++ src/components/common/home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false)
-    }, 500)
+    }, 1000)
 
     return () => clearTimeout(timer)
   }, [])
